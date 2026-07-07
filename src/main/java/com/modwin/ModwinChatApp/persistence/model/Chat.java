ef2b3a3e08de6f3ca5eae9d302e848d32b171729
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-07-07 17:11:38 +0200

    Updated basic skeleton to Chat class

--- src/main/java/com/modwin/ModwinChatApp/persistence/model/Chat.java
+++ src/main/java/com/modwin/ModwinChatApp/persistence/model/Chat.java
@@ -1,4 +1,28 @@
 package com.modwin.ModwinChatApp.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id",unique = true, nullable = false)
+    private Integer ID;
+
+    private Set<User> users;
+
+    private List<Message> messages;
+
+
+
 }
