e2f68495901ca3bdb5f41a48e2437b088e234e81
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-08-09 15:27:27 +0200

    Added FriendshipNotFoundException

---
+++ src/main/java/com/modwin/ModwinChatApp/exception/FriendshipNotFoundException.java
@@ -0,0 +1,6 @@
+package com.modwin.ModwinChatApp.exception;
+
+public class FriendshipNotFoundException extends RuntimeException {
+    public FriendshipNotFoundException(Integer friendshipId) {
+    }
+}
