1eb83081d925dcd27dffa4690c1dd99f33deebbe
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-08-12 17:09:28 +0200

    Updated to extend RuntineException instead.

--- src/main/java/com/modwin/ModwinChatApp/exception/UserAlreadyExistsException.java
+++ src/main/java/com/modwin/ModwinChatApp/exception/UserAlreadyExistsException.java
@@ -1,7 +1,7 @@
 package com.modwin.ModwinChatApp.exception;
 
 
-public class UserAlreadyExistsException extends IllegalArgumentException{
+public class UserAlreadyExistsException extends RuntimeException {
 
     public UserAlreadyExistsException(String msg){
         super(msg);
