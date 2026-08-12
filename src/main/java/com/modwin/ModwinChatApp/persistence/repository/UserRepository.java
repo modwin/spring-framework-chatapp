dce23e5e8c334645440babc1a814a6170fe18174
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-08-12 17:22:01 +0200

    Minor repo update.

--- src/main/java/com/modwin/ModwinChatApp/persistence/repository/UserRepository.java
+++ src/main/java/com/modwin/ModwinChatApp/persistence/repository/UserRepository.java
@@ -2,13 +2,18 @@ package com.modwin.ModwinChatApp.persistence.repository;
 
 import com.modwin.ModwinChatApp.persistence.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByUsername(String username);
+
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByEmail(String email);
-    void deleteByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
