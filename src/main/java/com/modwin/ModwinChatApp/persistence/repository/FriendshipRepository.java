3844cf4d1d737bc99f64052457e37e21985f42e0
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-08-12 17:21:35 +0200

    Minor repo query update.

--- src/main/java/com/modwin/ModwinChatApp/persistence/repository/FriendshipRepository.java
+++ src/main/java/com/modwin/ModwinChatApp/persistence/repository/FriendshipRepository.java
@@ -3,9 +3,12 @@ package com.modwin.ModwinChatApp.persistence.repository;
 import com.modwin.ModwinChatApp.persistence.model.Friendship;
 import com.modwin.ModwinChatApp.persistence.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FriendshipRepository extends JpaRepository<Friendship, Integer> {
@@ -16,4 +19,12 @@ public interface FriendshipRepository extends JpaRepository<Friendship, Integer>
                or (f.requester = :second and f.recipient = :first)
             """)
     boolean existsBetween(@Param("first") User first, @Param("second") User second);
+
+    @EntityGraph(attributePaths = {"requester", "recipient"})
+    @Query("""
+            select f from Friendship f
+            where f.requester.id = :userId or f.recipient.id = :userId
+            order by f.id desc
+            """)
+    List<Friendship> findAllForUser(@Param("userId") Integer userId);
 }
