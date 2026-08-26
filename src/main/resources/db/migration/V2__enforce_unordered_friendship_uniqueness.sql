278ab8ad795a949f375447630ce4362f8ca59350
ModerathoWinther <bjorn.mwinther@gmail.com>
2026-08-26 14:48:14 +0200

    Added flyway sql scripts

---
+++ src/main/resources/db/migration/V2__enforce_unordered_friendship_uniqueness.sql
@@ -0,0 +1,21 @@
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM friendship
+        GROUP BY LEAST(requester_id, recipient_id), GREATEST(requester_id, recipient_id)
+        HAVING COUNT(*) > 1
+    ) THEN
+        RAISE EXCEPTION
+            'Cannot enforce unordered friendship uniqueness: reciprocal duplicate rows exist.';
+    END IF;
+END
+$$;
+
+DROP INDEX IF EXISTS uk_friendship_pair;
+
+CREATE UNIQUE INDEX uk_friendship_pair
+    ON friendship (
+        LEAST(requester_id, recipient_id),
+        GREATEST(requester_id, recipient_id)
+    );
